a874422423ed0e796dfd8100a0a17fc1531ea2e2
leagan <45368414+leagan@users.noreply.github.com>
2018-12-13 11:14:17 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+	createCanvas(600, 600);
+	background(25,90,150);
+	fill(165,42,42);
+  ellipse(320,250,40,40);
+	fill(43, 30, 100);
+	ellipse(290,345,40,40);
+	fill(139,69,15)
+	translate(250,250)
+	beginShape(TRIANGLES)
+	vertex(30,75)
+	vertex(40,20)
+	vertex(50,75)
+	vertex(60,20)
+	vertex(70,75)
+	vertex(80,20)
+	endShape()
+}
+
+
+function draw() {
+	
+}
